7fca2aa2b5a937568826f0974620327c0e2dc3d6
alkos20 <lefrancq.lumierejll@gmail.com>
2023-03-08 12:19:29 +0100

    premier essai de commit sur une branche

---
+++ essai.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+print("Hello world")
+
+def somme(
+	a : int,
+	b : int,
+) -> int:
+	return a + b
+
+somme(7, 5)
+
+
+val_alice   =   15
+val_bob     =   20
+val_fred    =   120
+
+
+val_alice = 15
+val_bob   = 20
+val_fred  = 120
+
+
